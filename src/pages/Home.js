1903acbcc85525bbf691b646fc07f2f9b46bc922
hexie <hx3329@gmail.com>
2018-08-17 17:15:41 +1000

    build Body

---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {
+    Typography,
+} from '@material-ui/core';
+
+const Home = () => (
+    <Typography variant="display1">Welcome Home!</Typography>
+);
+export default Home;
