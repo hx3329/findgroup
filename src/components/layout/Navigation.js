f32cdeb6b95210148eb299d9e35cd6b96fbedcf8
hexie <hx3329@gmail.com>
2018-08-22 04:30:59 +1000

    improve navigation add icons

--- src/components/layout/Navigation.js
+++ src/components/layout/Navigation.js
@@ -1,8 +1,34 @@
 import React, {Component} from 'react';
-import { Navbar,NavItem,NavDropdown,MenuItem,Nav } from "react-bootstrap";
+import { Navbar,NavItem,NavDropdown,MenuItem,Nav, Button } from "react-bootstrap";
 import {Link} from "react-router-dom";
+import { withAuth } from '@okta/okta-react';
 
-class Navigation extends Component {
+export default withAuth(class Navigation extends Component {
+    state = { authenticated: null };
+
+    checkAuthentication = async() => {
+        const authenticated = await this.props.auth.isAuthenticated();
+        if (authenticated !== this.state.authenticated) {
+            this.setState({ authenticated });
+        }
+    }
+
+    async componentDidMount() {
+        this.checkAuthentication();
+    }
+
+    async componentDidUpdate() {
+        this.checkAuthentication();
+    }
+
+    login = async() => {
+        this.props.auth.login('/');
+    }
+
+    logout = async() => {
+        this.props.auth.logout('/');
+    }
+
     render(){
         return(
             //use react-bootstrap Navbar with collapse
@@ -16,7 +42,8 @@ class Navigation extends Component {
                 <Navbar.Collapse>
                     <Nav>
                         {/*use componentClass and href to fix the style error from combining bootstrap and router*/}
-                        <NavItem eventKey={1} componentClass={Link} href="/" to="/">Home</NavItem>
+                        <NavItem eventKey={1} componentClass={Link} href="/" to="/"><span
+                            className="glyphicon glyphicon-home"></span> Home</NavItem>
                         <NavItem eventKey={2} componentClass={Link} href="/about" to="about">About</NavItem>
                         <NavItem eventKey={2} componentClass={Link} href="/contact" to="contact">Contact</NavItem>
                         <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
@@ -28,17 +55,27 @@ class Navigation extends Component {
                         </NavDropdown>
                     </Nav>
                     <Nav pullRight>
-                        <NavItem eventKey={1} componentClass={Link} href="/myprofile" to="myprofile">Myprofile</NavItem>
-                        <NavItem eventKey={2} componentClass={Link} href="/login" to="login">
-                            Login
-                        </NavItem>
+                        <NavItem eventKey={1} componentClass={Link} href="/myprofile" to="myprofile"><span
+                            className="glyphicon glyphicon-user"></span> Myprofile</NavItem>
+                        {
+                            this.state.authenticated ?(
+                                <NavItem>
+                                    <Button bsSize="xs" onClick={this.logout}><span
+                                        className="glyphicon glyphicon-log-out"></span> logout</Button>
+                                </NavItem>
+                                ):(
+                                <NavItem>
+                                    <Button bsSize="xs" onClick={this.login}><span
+                                        className="glyphicon glyphicon-log-in"></span> Login</Button>
+                                </NavItem>)
+                        }
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
         )
     }
-}
+})
 
-export default Navigation;
+
 
 
